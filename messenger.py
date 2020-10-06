ca8df76efde9db30af1531319ffc444ece3f2853
Md. Masud Rana <53449312+MRSahidShorkar@users.noreply.github.com>
2020-10-06 14:09:59 +0600

    Message Bot

---
+++ messenger.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+message = 15
+
+while message>0:
+    time.sleep(2)
+    pyautogui.typewrite(" dstw message bot program create kore felsi hehehehhe.")
+    time.sleep(1)
+    pyautogui.press("enter")
+    message = message - 1
